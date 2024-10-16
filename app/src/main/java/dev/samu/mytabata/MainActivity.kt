c68f7a372e43e24817fea025aa003e04a70bab3c
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-10-16 08:19:45 +0100

    Actualizado android studio y modificado importaciones

--- app/src/main/java/dev/samu/mytabata/MainActivity.kt
+++ app/src/main/java/dev/samu/mytabata/MainActivity.kt
@@ -20,8 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import dev.samu.mytabata.ui.theme.MytabataTheme
 
 var counterState : Boolean = false
