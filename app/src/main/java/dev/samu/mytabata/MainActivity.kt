c43ff0a2c9a78d0f426d8e95f68fafa4711a7fad
SamuelRiveira <160497907+SamuelRiveira@users.noreply.github.com>
2024-10-17 19:25:32 +0100

    Counter Terminado

--- app/src/main/java/dev/samu/mytabata/MainActivity.kt
+++ app/src/main/java/dev/samu/mytabata/MainActivity.kt
@@ -1,10 +1,10 @@
 package dev.samu.mytabata
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,29 +12,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TimeInput
-import androidx.compose.material3.TimePicker
-import androidx.compose.material3.TimePickerState
-import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key.Companion.Calendar
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 import dev.samu.mytabata.ui.theme.MytabataTheme
 
 class MainActivity : ComponentActivity() {
@@ -223,11 +215,19 @@ fun PantallaPrincipal(
 }
 
 @Composable
-fun Counter(modifier: Modifier = Modifier) {
+fun Counter(
+    modifier: Modifier = Modifier
+) {
     var theCounter by remember { mutableStateOf("00:00") }
-    val miCounterDown = remember { CounterDown(99) { newValue ->
-        theCounter = miCounterDown.getFormattedTime()
-    } }
+
+    var texto by remember { mutableStateOf("Start") }
+
+    // Usamos `remember` para crear y mantener `miCounterDown` durante el ciclo de vida del Composable
+    val miCounterDown = remember {
+        CounterDown(99) { newValue ->
+            theCounter = newValue  // Aquí asignamos el tiempo formateado directamente
+        }
+    }
 
     Column(
         modifier = Modifier
@@ -241,9 +241,15 @@ fun Counter(modifier: Modifier = Modifier) {
             modifier = modifier
         )
         Button(onClick = {
+            Log.i("error", "Botón pulsado")
             miCounterDown.toggle()
+            if (texto == "Start"){
+                texto = "Pause"
+            } else{
+                texto = "Start"
+            }
         }) {
-            Text(text = if (miCounterDown.isStarted) "Pause" else "Start")
+            Text(text = texto)
         }
     }
 }
