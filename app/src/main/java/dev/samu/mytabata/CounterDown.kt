c4bda7f67331f909098fb1f7498f98683c9111c3
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-10-17 12:26:29 +0100

    Pantalla principal

--- app/src/main/java/dev/samu/mytabata/CounterDown.kt
+++ app/src/main/java/dev/samu/mytabata/CounterDown.kt
@@ -1,20 +1,76 @@
 package dev.samu.mytabata
 
 import android.os.CountDownTimer
+import android.util.Log
 
-class CounterDown(var segundos: Int, val onCounterTick: (Long) -> Unit) {
-    var myCounter: CountDownTimer
-    var counterState: Boolean = false
+class CounterDown(var segundos: Int, var loquehacealhacertick: (Long) -> Unit) {
+    private var counterState: Boolean = false
+    private var remainingTime: Long = (segundos * 1000L)
+    private var isStarted: Boolean = false
+
+    private var myCounter: CountDownTimer? = null
 
     init {
-        myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
+        myCounter = object : CountDownTimer(remainingTime, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                onCounterTick(millisUntilFinished / 1000)
+                remainingTime = millisUntilFinished
+                if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
             }
 
             override fun onFinish() {
                 counterState = false
+                isStarted = false
+                remainingTime = 0
+                Log.i("dam2", "mensajito")
             }
         }
+    }
+
+    fun toggle() {
+        Log.i("dam2", "toggle: $counterState")
+        if (this.counterState) {
+            this.cancel()
+        } else {
+            if (!isStarted) {
+                Log.i("dam2", "toggle: start")
+                start()
+                isStarted = true
+            } else {
+                resume()
+            }
+        }
+    }
+
+    fun start() {
+        counterState = true
+        myCounter?.start()
+    }
+
+    fun cancel() {
+        counterState = false
+        myCounter?.cancel()
+    }
+
+    fun resume() {
+        counterState = true
+        myCounter = object : CountDownTimer(remainingTime, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                remainingTime = millisUntilFinished
+                if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
+            }
+
+            override fun onFinish() {
+                counterState = false
+                isStarted = false
+                remainingTime = 0
+                Log.i("dam2", "mensajito")
+            }
+        }.start()
+    }
+
+    fun getFormattedTime(): String {
+        val minutes = (remainingTime / 1000) / 60
+        val seconds = (remainingTime / 1000) % 60
+        return String.format("%02d:%02d", minutes, seconds)
     }
 }
--- app/src/main/java/dev/samu/mytabata/MainActivity.kt
+++ app/src/main/java/dev/samu/mytabata/MainActivity.kt
@@ -1,25 +1,40 @@
-// MainActivity.kt
 package dev.samu.mytabata
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.Calendar
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.samu.mytabata.ui.theme.MytabataTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +44,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             MytabataTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Counter(
+//                    PantallaPrincipal (
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                    Counter (
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -39,10 +57,177 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Counter(modifier: Modifier) {
-    var theCounter by remember { mutableLongStateOf(0L) }
+fun PantallaPrincipal(
+    modifier: Modifier
+){
+    var timeReset by remember { mutableStateOf(0) }
+    var timeSet by remember { mutableStateOf(0) }
+    var timeWork by remember { mutableStateOf(0) }
 
-    val miCounterDown = CounterDown(99, { newCounterValue -> theCounter = newCounterValue})
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(text = "Tabata", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        ) {
+            Row {
+                Column(
+                    modifier = Modifier
+                        .padding(bottom = 50.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(text = "Sets", fontSize = 25.sp)
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceAround,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            modifier = Modifier.run {
+                                clickable{
+                                    if (timeSet > 0){
+                                        timeSet--
+                                    }
+                                }
+                                .padding(10.dp)
+                            },
+                            text = "-",
+                            fontSize = 25.sp
+                        )
+                        Text(text = "$timeSet", fontSize = 25.sp)
+                        Text(
+                            modifier = Modifier.run {
+                                clickable{
+                                    timeSet++
+                                }
+                                .padding(10.dp)
+                            },
+                            text = "+",
+                            fontSize = 25.sp
+                        )
+                    }
+                }
+            }
+            Row {
+                Column(
+                    modifier = Modifier
+                        .padding(bottom = 50.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(text = "Work", fontSize = 25.sp)
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceAround,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            modifier = Modifier.run {
+                                clickable{
+                                    if (timeWork > 0){
+                                        timeWork--
+                                    }
+                                }
+                                    .padding(10.dp)
+                            },
+                            text = "-",
+                            fontSize = 25.sp
+                        )
+                        Text(text = "$timeWork", fontSize = 25.sp)
+                        Text(
+                            modifier = Modifier.run {
+                                clickable{
+                                    timeWork++
+                                }
+                                    .padding(10.dp)
+                            },
+                            text = "+",
+                            fontSize = 25.sp
+                        )
+                    }
+                }
+            }
+            Row {
+                Column(
+                    modifier = Modifier
+                        .padding(bottom = 50.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(text = "Reset", fontSize = 25.sp)
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceAround,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            modifier = Modifier.run {
+                                clickable{
+                                    if (timeReset > 0){
+                                        timeReset--
+                                    }
+                                }
+                                    .padding(10.dp)
+                            },
+                            text = "-",
+                            fontSize = 25.sp
+                        )
+                        Text(text = "$timeReset", fontSize = 25.sp)
+                        Text(
+                            modifier = Modifier.run {
+                                clickable{
+                                    timeReset++
+                                }
+                                    .padding(10.dp)
+                            },
+                            text = "+",
+                            fontSize = 25.sp
+                        )
+                    }
+                }
+            }
+            Row {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    onClick = {}
+                ) {
+                    Text(text = "Start", fontSize = 20.sp)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Counter(modifier: Modifier = Modifier) {
+    var theCounter by remember { mutableStateOf("00:00") }
+    val miCounterDown = remember { CounterDown(99) { newValue ->
+        theCounter = miCounterDown.getFormattedTime()
+    } }
 
     Column(
         modifier = Modifier
@@ -50,24 +235,24 @@ fun Counter(modifier: Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row {
-            Text(text = theCounter.toString())
+        Text(
+            text = theCounter,
+            fontSize = 50.sp,
+            modifier = modifier
+        )
+        Button(onClick = {
+            miCounterDown.toggle()
+        }) {
+            Text(text = if (miCounterDown.isStarted) "Pause" else "Start")
         }
-        Row {
-            Button(
-                modifier = Modifier,
-                onClick = {
-                    if (!miCounterDown.counterState) {
-                        miCounterDown.myCounter.start()
-                        miCounterDown.counterState = true
-                    } else {
-                        miCounterDown.myCounter.cancel()
-                        miCounterDown.counterState = false
-                    }
-                }
-            ) {
-                Text(text = if (miCounterDown.counterState) "Parar" else "Iniciar")
-            }
-        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TabataPreview() {
+    MytabataTheme {
+//        PantallaPrincipal(modifier = Modifier)
+        Counter(modifier = Modifier)
     }
 }
